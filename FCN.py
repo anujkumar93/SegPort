497a750ea9372f56a84493cf35c6f2c2d5bd2485
Sudhamsu Krishna <smanne@cs.umass.edu>
2018-04-24 00:19:45 -0400

    Memory optimizations (also switch from Adam to SGD)

--- FCN.py
+++ FCN.py
@@ -6,48 +6,35 @@ class FCN(nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.relu = nn.ReLU(inplace=True)
+
         self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=100)
-        self.relu1_1 = nn.ReLU(inplace=True)
         self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.relu1_2 = nn.ReLU(inplace=True)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.relu2_1 = nn.ReLU(inplace=True)
         self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.relu2_2 = nn.ReLU(inplace=True)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.relu3_1 = nn.ReLU(inplace=True)
         self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.relu3_2 = nn.ReLU(inplace=True)
         self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.relu3_3 = nn.ReLU(inplace=True)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        self.relu4_1 = nn.ReLU(inplace=True)
         self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.relu4_2 = nn.ReLU(inplace=True)
         self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.relu4_3 = nn.ReLU(inplace=True)
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.relu5_1 = nn.ReLU(inplace=True)
         self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.relu5_2 = nn.ReLU(inplace=True)
         self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.relu5_3 = nn.ReLU(inplace=True)
         self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.conv6 = nn.Conv2d(512, 4096, kernel_size=7)
-        self.relu6 = nn.ReLU(inplace=True)
         self.drop6 = nn.Dropout2d(p=0.5, inplace=True)
 
         self.fc7 = nn.Conv2d(4096, 4096, kernel_size=1)
-        self.relu7 = nn.ReLU(inplace=True)
         self.drop7 = nn.Dropout2d(p=0.5, inplace=True)
 
         self.score = nn.Conv2d(4096, 1, kernel_size=1)
@@ -61,37 +48,33 @@ class FCN(nn.Module):
         self.score_deconv3 = nn.ConvTranspose2d(1, 1, kernel_size=16, stride=8, bias=False)
 
     def forward(self, batch):
-        output_pool1 = self.pool1(self.relu1_2(self.conv1_2(
-                                  self.relu1_1(self.conv1_1(batch)))))
-        output_pool2 = self.pool2(self.relu2_2(self.conv2_2(
-                                  self.relu2_1(self.conv2_1(output_pool1)))))
-        output_pool3 = self.pool3(self.relu3_3(self.conv3_3(
-                                  self.relu3_2(self.conv3_2(
-                                  self.relu3_1(self.conv3_1(output_pool2)))))))
-        output_pool4 = self.pool4(self.relu4_3(self.conv4_3(
-                                  self.relu4_2(self.conv4_2(
-                                  self.relu4_1(self.conv4_1(output_pool3)))))))
-        output_pool5 = self.pool5(self.relu5_3(self.conv5_3(
-                                  self.relu5_2(self.conv5_2(
-                                  self.relu5_1(self.conv5_1(output_pool4)))))))
-        output_drop6 = self.drop6(self.relu6(self.conv6(output_pool5)))
-        output_drop7 = self.drop7(self.relu7(self.fc7(output_drop6)))
-        output_score = self.score(output_drop7)
+        output_pool3 = self.pool1(self.relu(self.conv1_2(
+                                  self.relu(self.conv1_1(batch)))))
+        output_pool3 = self.pool2(self.relu(self.conv2_2(
+                                  self.relu(self.conv2_1(output_pool3)))))
+        output_pool3 = self.pool3(self.relu(self.conv3_3(
+                                  self.relu(self.conv3_2(
+                                  self.relu(self.conv3_1(output_pool3)))))))
+        output_pool4 = self.pool4(self.relu(self.conv4_3(
+                                  self.relu(self.conv4_2(
+                                  self.relu(self.conv4_1(output_pool3)))))))
+        output_score = self.pool5(self.relu(self.conv5_3(
+                                  self.relu(self.conv5_2(
+                                  self.relu(self.conv5_1(output_pool4)))))))
+        output_score = self.drop6(self.relu(self.conv6(output_score)))
+        output_score = self.drop7(self.relu(self.fc7(output_score)))
+        output_score = self.score(output_score)
 
-        output_score_deconv1 = self.score_deconv1(output_score)
-        output_score_pool4 = self.score_pool4(output_pool4)
-        output_score_pool4_cropped = self.crop(output_score_pool4, output_score_deconv1)
-        output_score_fused1 = output_score_deconv1 + output_score_pool4_cropped
+        output_score = self.score_deconv1(output_score)
+        output_score += self.crop(self.score_pool4(output_pool4), output_score)
 
-        output_score_deconv2 = self.score_deconv2(output_score_fused1)
-        output_score_pool3 = self.score_pool3(output_pool3)
-        output_score_pool3_cropped = self.crop(output_score_pool3, output_score_deconv2)
-        output_score_fused2 = output_score_deconv2 + output_score_pool3_cropped
+        output_score = self.score_deconv2(output_score)
+        output_score += self.crop(self.score_pool3(output_pool3), output_score)
 
-        output_score_deconv3 = self.score_deconv3(output_score_fused2)
-        output_score_final = self.crop(output_score_deconv3, batch)
+        output_score = self.score_deconv3(output_score)
+        output_score = self.crop(output_score, batch)
 
-        return output_score_final
+        return output_score
 
     @staticmethod
     def crop(from_mat, to_mat):
--- train.py
+++ train.py
@@ -13,7 +13,8 @@ REG = 1e-4
 
 def train(epochs=EPOCHS, batch_size=BATCH_SIZE, lr=LR, reg=REG):
     model = FCN()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=reg)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=reg)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=reg)
     loss_function = nn.BCEWithLogitsLoss()
 
     dlo = DataLoader(batch_size)
@@ -27,12 +28,13 @@ def train(epochs=EPOCHS, batch_size=BATCH_SIZE, lr=LR, reg=REG):
             model.zero_grad()
             i += batch_size
             x, y = dlo.generate_batch()
-            x_var = torch.autograd.Variable(torch.FloatTensor(x))
-            y_var = torch.autograd.Variable(torch.FloatTensor(y))
-            output = model(x_var)
-            loss = loss_function(output, y_var)
+            x = torch.autograd.Variable(torch.FloatTensor(x))
+            y = torch.autograd.Variable(torch.FloatTensor(y))
+            output = model(x)
+            loss = loss_function(output, y)
             loss.backward()
             optimizer.step()
             print(loss.data[0])
+            del x, y, output, loss
 
     return model
